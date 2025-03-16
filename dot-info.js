3c2268e2a7af0e11853dd22c54bcce0ecf2876ea
Sravan kumar polisetti <111481159+10srav@users.noreply.github.com>
2025-03-16 10:22:36 +0530

    Add files via upload

---
+++ dot-info.js
@@ -0,0 +1,26 @@
+const contactBtn = document.getElementById('contactNowBtn'); 
+const navLinks = document.querySelectorAll('nav ul li a');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function(e) {
+    e.preventDefault();
+    const targetId = this.getAttribute('href');
+    const targetElement = document.querySelector(targetId);
+    
+    if (targetElement) {
+      targetElement.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
+
+const contactForm = document.getElementById('contactForm');
+
+contactBtn.addEventListener('click', () => { 
+
+  if (contactForm.style.display === 'none' || contactForm.style.display === '') {
+    contactForm.style.display = 'block';
+  } else {
+    contactForm.style.display = 'none';
+  }
+});
+h
